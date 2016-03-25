09bd8a219e29909812bdcd351315b5f37ddb1eaa
Patrick Seelinger <Patrick Seelinger>
2016-03-25 01:13:53 -0400

    Need to add a footer. Header and menu work great

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,3 +2,9 @@ $(".nav-side .nav-toggle").on("click", function(e) {
   e.preventDefault();
   $(this).parent().toggleClass("nav-open");
 });
+function highlight(link){
+  document.getElementById(link).style.color = 'cornflowerblue';
+};
+function unhighlight(link){
+  document.getElementById(link).style.color = 'white';
+}
